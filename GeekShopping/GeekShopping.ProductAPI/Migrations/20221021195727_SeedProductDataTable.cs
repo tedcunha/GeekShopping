03a23f66f562678b4e4658306e37e07707227bb2
Ricardo Cunha <rcunha@britech.global>
2022-10-21 17:05:12 -0300

    Secao8(L812)-Executando os Seeds (Migrations e Populando o  Banco de Dados)

---
+++ GeekShopping/GeekShopping.ProductAPI/Migrations/20221021195727_SeedProductDataTable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 1L, "CANECA", "Caneca R2D2", "D:\\Ricardo\\Cursos\\Udemy\\GeekShopping\\GeekShopping\\GeekShopping.ProductAPI\\Image\\41MIxdiEcDL._AC_.jpg", "Caneca R2D2", 39.9m },
+                    { 2L, "CANECA", "Caneca R2D2", "D:\\Ricardo\\Cursos\\Udemy\\GeekShopping\\GeekShopping\\GeekShopping.ProductAPI\\Image\\41MIxdiEcDL._AC_.jpg", "Caneca R2D2", 39.9m },
+                    { 3L, "CANECA", "Caneca R2D2", "D:\\Ricardo\\Cursos\\Udemy\\GeekShopping\\GeekShopping\\GeekShopping.ProductAPI\\Image\\41MIxdiEcDL._AC_.jpg", "Caneca R2D2", 39.9m },
+                    { 4L, "CANECA", "Caneca R2D2", "D:\\Ricardo\\Cursos\\Udemy\\GeekShopping\\GeekShopping\\GeekShopping.ProductAPI\\Image\\41MIxdiEcDL._AC_.jpg", "Caneca R2D2", 39.9m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+        }
+    }
+}
