f6eab559861e040f6f9c0dac6dd17ea60257ed32
Ricardo Cunha <rcunha@britech.global>
2022-10-21 10:57:08 -0300

    Atualizando o GitIgnore

---
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,50 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Configurando conecton no banco
+var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0, 23)))
+);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+// Configurando o AutoMapper
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Injeção de Dependencia
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+// Adicionando Configurações Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MySQLContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductRepository(MySQLContext context,
+                                 IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductVO>> FindAll()
+        {
+            // Indo no banco e fazendo o Bind da tabela Products para o Objeto Product
+            List<Product> products = await _context.Products.ToListAsync();
+            // Convertendo da Entidade Products para o VO ProductVO
+            return _mapper.Map<List<ProductVO>>(products);
+        }
+
+        public async Task<ProductVO> FindById(long id)
+        {
+            // Indo no banco e fazendo o Bind da tabela Products para o Objeto Product
+            Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+            // Convertendo da Entidade Products para o VO ProductVO
+            return _mapper.Map<ProductVO>(product);
+        }
+
+        public async Task<ProductVO> Create(ProductVO vo)
+        {
+            // Convertido para Entidade Product
+            Product product = _mapper.Map<Product>(vo);
+            // Adicionando 
+            _context.Products.Add(product);
+            // Salvando
+            await _context.SaveChangesAsync();
+            // Devolvendo um VO
+            return _mapper.Map<ProductVO>(product);
+        }
+        public async Task<ProductVO> Update(ProductVO vo)
+        {
+            // Convertido para Entidade Product
+            Product product = _mapper.Map<Product>(vo);
+            // Alterando
+            _context.Products.Update(product);
+            // Salvando
+            await _context.SaveChangesAsync();
+            // Devolvendo um VO
+            return _mapper.Map<ProductVO>(product);
+        }
+        public async Task<bool> Delete(long id)
+        {
+            try
+            {
+                // Indo no banco e fazendo o Bind da tabela Products para o Objeto Product
+                Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+                if (product == null) return false;
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
