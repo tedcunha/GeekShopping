7f7fbf06f4c08197cf0eefec8dd92b054a377c66
Ricardo Cunha <rcunha@britech.global>
2022-10-21 16:45:10 -0300

    Secao8(L0811)-Cinfigurando o Seed Data no Microsservico de Produtos

--- GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -7,5 +7,45 @@ namespace GeekShopping.ProductAPI.Model.Context
         public MySQLContext() { }
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 1,
+                Name = "Caneca R2D2",
+                Description = "Caneca R2D2",
+                CategoryName = "CANECA",
+                Price = new decimal(39.90),
+                ImageURL = "D:\\Ricardo\\Cursos\\Udemy\\GeekShopping\\GeekShopping\\GeekShopping.ProductAPI\\Image\\41MIxdiEcDL._AC_.jpg"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                Name = "Caneca R2D2",
+                Description = "Caneca R2D2",
+                CategoryName = "CANECA",
+                Price = new decimal(39.90),
+                ImageURL = "D:\\Ricardo\\Cursos\\Udemy\\GeekShopping\\GeekShopping\\GeekShopping.ProductAPI\\Image\\41MIxdiEcDL._AC_.jpg"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Caneca R2D2",
+                Description = "Caneca R2D2",
+                CategoryName = "CANECA",
+                Price = new decimal(39.90),
+                ImageURL = "D:\\Ricardo\\Cursos\\Udemy\\GeekShopping\\GeekShopping\\GeekShopping.ProductAPI\\Image\\41MIxdiEcDL._AC_.jpg"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Caneca R2D2",
+                Description = "Caneca R2D2",
+                CategoryName = "CANECA",
+                Price = new decimal(39.90),
+                ImageURL = "D:\\Ricardo\\Cursos\\Udemy\\GeekShopping\\GeekShopping\\GeekShopping.ProductAPI\\Image\\41MIxdiEcDL._AC_.jpg"
+            });
+        }
     }
 }
