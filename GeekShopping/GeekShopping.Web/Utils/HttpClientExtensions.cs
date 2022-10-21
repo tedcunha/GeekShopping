02d7ef64cc13254a37c391fdf18ed9253f8089aa
Ricardo Cunha <rcunha@britech.global>
2022-10-21 18:56:28 -0300

    Secao9(L0904)-Implementando os metodos PostASJson e PutAsJson em HttpClientExtensions

---
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response) 
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Alguma deu errado na chamada a API: {response.ReasonPhrase}");
+            }
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
